d596e0569d2012defee1b27ee1502ff62eccbd60
Suresh021 <ssc760951@gmail.com>
2024-01-19 11:07:00 +0530

    secondCommit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pokemon/login.dart';
+import 'package:pokemon/main.dart';
 
 
 void main() {
